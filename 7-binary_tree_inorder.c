1ed521a29baca80bb55e05dba8ba4c04013c9ca9
Jesufemi <oladapofemmy@gmail.com>
2024-02-28 22:09:35 +0100

    fix 7,8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to function to call for each node
  *
  * Return: void
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * binary_tree_postorder - goes through a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to function to call for each node
  *
  * Return: void
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
